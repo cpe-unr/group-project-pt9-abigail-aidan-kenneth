d139b2295ef51cb1af56de5f9ada241810082b5d
Abigail Calvelo <abigailcalvelo@yahoo.com>
2021-05-02 09:36:41 -0700

    metadata start -ABBY

--- WaveHeader.h
+++ WaveHeader.h
@@ -2,6 +2,11 @@
 #ifndef WAVEHEADER_H
 #define WAVEHEADER_H
 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdint> // for the uint's
+
 // This header copied from https://gist.github.com/Jon-Schneider/8b7c53d27a7a13346a643dac9c19d34f
 /*
  * https://docs.fileformat.com/audio/wav/
@@ -43,6 +48,29 @@ typedef struct wav_header {
 
     /*
         Metadata comes after this point
+
+// #pragma pack(push,2) 
+typedef struct Header_WAV
+{
+    // public:
+        /* RIFF Chunk Descriptor */
+        uint8_t         RIFF[4];        // RIFF Header Magic header 
+        uint32_t        ChunkSize;      // RIFF Chunk Size
+        uint8_t         WAVE[4];        // WAVE Header
+        /* "fmt" sub-chunk */
+        uint8_t         fmt[4];         // FMT header
+        uint32_t        Subchunk1Size;  // Size of the fmt chunk
+        uint16_t        AudioFormat;    // Audio format 1=PCM,6=mulaw,7=alaw,     257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
+        uint16_t        NumOfChan;      // Number of channels 1=Mono 2=Sterio
+        uint32_t        SamplesPerSec;  // Sampling Frequency in Hz
+        uint32_t        bytesPerSec;    // bytes per second
+        uint16_t        blockAlign;     // 2=16-bit mono, 4=16-bit stereo
+        uint16_t        bitsPerSample;  // Number of bits per sample
+        /* "data" sub-chunk */
+        uint8_t         Subchunk2ID[4]; // "data"  string
+        uint32_t        Subchunk2Size;  // Sampled data length
+//} Header_WAV;
+
    */
 } wav_header;
 #endif //WAVEHEADER_H
